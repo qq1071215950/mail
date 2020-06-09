e40664d7f802f8f59e063cbe23da2a906bcaef9b
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-09 15:24:54 +0800

    集成定时任务框架

---
+++ src/main/java/com/haojing/mail/config/SpringTaskConfig.java
@@ -0,0 +1,14 @@
+package com.haojing.mail.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 开始定时任务
+ * @author jiange
+ * @date 2020/6/9 15:22
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
