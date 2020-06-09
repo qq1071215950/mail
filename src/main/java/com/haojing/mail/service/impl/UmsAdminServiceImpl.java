a1b3b12903624897e4f2775029fa87fa00c9990c
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-09 11:42:56 +0800

    加入security的安全配置类

---
+++ src/main/java/com/haojing/mail/service/impl/UmsAdminServiceImpl.java
@@ -0,0 +1,56 @@
+package com.haojing.mail.service.impl;
+
+import com.haojing.mail.common.utils.JwtTokenUtil;
+import com.haojing.mail.mbg.mapper.UmsAdminMapper;
+import com.haojing.mail.mbg.model.UmsAdmin;
+import com.haojing.mail.mbg.model.UmsPermission;
+import com.haojing.mail.service.UmsAdminService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author jiange
+ * @date 2020/6/9 11:28
+ */
+@Service
+public class UmsAdminServiceImpl implements UmsAdminService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UmsAdminServiceImpl.class);
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Value("${jwt.tokenHead}")
+    private String tokenHead;
+    @Autowired
+    private UmsAdminMapper adminMapper;
+
+    @Override
+    public UmsAdmin getAdminByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public UmsAdmin register(UmsAdmin umsAdminParam) {
+        return null;
+    }
+
+    @Override
+    public String login(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public List<UmsPermission> getPermissionList(Long adminId) {
+        return null;
+    }
+}
