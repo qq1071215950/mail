e869ccbc883f77de5ce0c14cb6fcac3e3a9f2897
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-10 15:25:53 +0800

    集成aop，进行日志监控

---
+++ src/main/java/com/haojing/mail/dto/WebLog.java
@@ -0,0 +1,69 @@
+package com.haojing.mail.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Controller层的日志封装类
+ * @author jiange
+ * @date 2020/6/10 15:18
+ */
+@Getter
+@Setter
+public class WebLog {
+
+    /**
+     * 操作描述
+     */
+    private String description;
+
+    /**
+     * 操作用户
+     */
+    private String username;
+
+    /**
+     * 操作时间
+     */
+    private Long startTime;
+
+    /**
+     * 消耗时间
+     */
+    private Integer spendTime;
+
+    /**
+     * 根路径
+     */
+    private String basePath;
+
+    /**
+     * URI
+     */
+    private String uri;
+
+    /**
+     * URL
+     */
+    private String url;
+
+    /**
+     * 请求类型
+     */
+    private String method;
+
+    /**
+     * IP地址
+     */
+    private String ip;
+
+    /**
+     * 请求参数
+     */
+    private Object parameter;
+
+    /**
+     * 请求返回的结果
+     */
+    private Object result;
+}
